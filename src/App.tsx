ad54de2793cd224247ac14c4e8f5a83803911fe5
Rangga-PR <ranggaputraraharja@gmail.com>
2022-01-08 19:40:44 +0700

    update app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
+import DigitalClock from "./components/DigitalClock/DigitalClock";
+import Layout from "./Layout";
+import Search from "./components/Search/Search";
+import logo from "./logo.svg";
+import styles from "./App.module.css";
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <section class={styles.header}>
+        <Search />
+      </section>
+      <section class={styles.content}>
+        <DigitalClock />
+      </section>
+      <section></section>
+    </Layout>
   );
 };
 
